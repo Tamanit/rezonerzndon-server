7749ad500e0d1172818463931193591c66575d21
Plohih Alex. <plohih215@ya.ru>
2024-09-07 02:10:36 +0500

    t

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be25b7a9ee224a516bee10ab431bdca357feedb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da6e42dbd9fe5121f63bc344c885abf38ae34e6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

dc2f8857622e3c971559cd369381d745b796cacf
Plohih Alex. <plohih215@ya.ru>
2024-09-07 02:23:32 +0500

    e

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
 {
-    builder.WithOrigins("https://rezone-random.ru/").AllowAnyMethod().AllowAnyHeader();
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
 }));
 
 // Add services to the container.
